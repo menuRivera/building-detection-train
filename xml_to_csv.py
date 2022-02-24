9abbbc692ca002b5e36215835be8570d79ce9b1a
Manuel Rivera <menu.rivera@gmail.com>
2022-02-24 16:28:00 -0600

    [MOD] -> xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -23,16 +23,18 @@ def xml_to_csv(path):
                      int(member[4][3].text)
                      )
             xml_list.append(value)
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    column_name = ['filename', 'width', 'height',
+                   'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
 
 def main():
     for folder in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
-        xml_df = xml_to_csv(image_path) # ej. images/train
-        xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None) # genera el .csv de cada folder
+        image_path = '/content/building-detection-train/images/' + folder
+        xml_df = xml_to_csv(image_path)  # ej. images/train
+        # genera el .csv de cada folder
+        xml_df.to_csv(('/content/building-detection-train/images/'+folder+'_labels.csv'), index=None)
     print('Successfully converted xml to csv.')
 
 
